82dfb5601134699481c94a0bc1ac0b8590627286
Marina Domashenko <creator.marina@gmail.com>
2020-09-23 00:45:13 +0300

    Added task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -7,7 +7,8 @@
 //   id="validation-input"
 //   data-length="6"
 //   placeholder="Введи 6 символов"
-// /> Сколько символов должно быть в инпуте, указывается в его атрибуте
+// />
+// Сколько символов должно быть в инпуте, указывается в его атрибуте
 // data-length. Если введено подходящее количество, то border инпута становится
 // зеленым, если неправильное - красным. Для добавления стилей, используй
 // CSS-классы valid и invalid.
@@ -17,3 +18,23 @@
 // #validation-input.valid { border-color: #4caf50; }
 
 // #validation-input.invalid { border-color: #f44336; }
+
+const input = document.querySelector('#validation-input');
+// input.addEventListener('focus', onInputFocus);
+input.addEventListener('blur', onInputBlur);
+
+function onInputBlur() {
+  if (input.value.length === +input.dataset.length) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    //   console.log(event.currentTarget.value);
+    //   if (input.value.length !== input.dataset.length) {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
+  }
+}
+
+// function onInputBlur(event) {
+//   console.log(event.currentTarget.value);
+// }
